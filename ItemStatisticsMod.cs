305bf7152c12fa0d4072cee57453b88178715698
GiometryE <40405764+GiometryE@users.noreply.github.com>
2018-06-19 21:35:20 +0200

    Add files via upload

---
+++ ItemStatisticsMod.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace ItemStatistics
+{
+    public class ItemStatisticsMod : IUserMod
+    {
+        public string Name => "Show Limits";
+        public string Description => "Displays the number of buildings and trees in your city (Hotkey CTRL + I)";
+    }
+}
---
+++ ItemStatisticsThreading.cs
@@ -0,0 +1,40 @@
+﻿using ColossalFramework.UI;
+using ICities;
+using UnityEngine;
+
+namespace ItemStatistics
+{
+    public class ItemStatisticsThreading : ThreadingExtensionBase
+    {
+        private bool _processed = false;
+
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
+        {
+            if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKey(KeyCode.I))
+            {
+                // cancel if they key input was already processed in a previous frame
+                if (_processed) return;
+
+                _processed = true;
+
+                // compose message
+                int treeCount = TreeManager.instance.m_treeCount;
+                int maxTreeCount = TreeManager.MAX_TREE_COUNT;
+
+                int buildingCount = BuildingManager.instance.m_buildingCount;
+                int maxBuildingCount = BuildingManager.MAX_BUILDING_COUNT;
+
+                string message = $"Trees: {treeCount} of {maxTreeCount}\nBuildings: {buildingCount} of {maxBuildingCount}";
+
+                // display message
+                ExceptionPanel panel = UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel");
+                panel.SetMessage("Item Statistics", message, false);
+            }
+            else
+            {
+                // not both keys pressed: Reset processed state
+                _processed = false;
+            }
+        }
+    }
+}
